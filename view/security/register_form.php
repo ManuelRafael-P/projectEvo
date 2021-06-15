f52d47c69551680ceffd6d7b15993e82e34766f1
Waffles4712 <armstrong4712@gmail.com>
2021-06-15 17:55:04 -0500

    Validaciones de registro completas

--- view/security/register_form.php
+++ view/security/register_form.php
@@ -5,7 +5,7 @@
                 <div class="card  card-signin my-5">
                     <div class="card-body">
                         <h5 class="card-title text-center">Registro de nuevo usuario</h5>
-                        <form class="form-signin" method="post" action="?c=security&a=registerUserTypeClient">
+                        <form class="form-signin" method="post" action="?c=security&a=registerUserTypeClient" oninput='inputRepeatPassword.setCustomValidity(inputRepeatPassword.value != inputPassword.value ? "Las contraseñas no coinciden." : "")' >
                             <?php if ($message) {
                                 echo ("<p>Registro exitoso.</p>");
                             } else if ($error) {
@@ -15,43 +15,43 @@
                             <div class="row">
                                 <div class="col-sm-6">
                                     <div class="form-label-group">
-                                        <input type="text" id="inputNames" name="inputNames" class="form-control" placeholder="Names address" onkeydown="return /[a-z ]/i.test(event.key)" autofocus>
+                                        <input type="text" id="inputNames" name="inputNames" class="form-control" placeholder="Names address" onkeydown="return /[a-z ]/i.test(event.key)" autofocus required>
                                         <label for="inputNames">Nombres</label>
                                     </div>
                                 </div>
                                 <div class="col-sm-6">
                                     <div class="form-label-group">
-                                        <input type="text" id="inputSurNames" name="inputSurNames" class="form-control" placeholder="SurNames" onkeydown="return /[a-z ]/i.test(event.key)" autofocus>
+                                        <input type="text" id="inputSurNames" name="inputSurNames" class="form-control" placeholder="SurNames" onkeydown="return /[a-z ]/i.test(event.key)" autofocus required>
                                         <label for="inputSurNames">Apellidos</label>
                                     </div>
                                 </div>
                                 <div class="col-sm-12">
                                     <div class="form-label-group">
-                                        <input type="email" id="inputEmail" name="inputEmail" class="form-control" placeholder="Email address" autofocus>
+                                        <input type="email" id="inputEmail" name="inputEmail" class="form-control" placeholder="Email address" autofocus required>
                                         <label for="inputEmail">Correo electronico</label>
                                     </div>
                                 </div>
                                 <div class="col-sm-6">
                                     <div class="form-label-group">
-                                        <input type="Phone" id="inputPhone" name="inputPhone" class="form-control" placeholder="Mobile phone" maxlength="9" autofocus>
+                                        <input type="Phone" id="inputPhone" name="inputPhone" class="form-control" placeholder="Mobile phone" minlength="9" autofocus required>
                                         <label for="inputPhone">Telefono celular</label>
                                     </div>
                                 </div>
                                 <div class="col-sm-6">
                                     <div class="form-label-group">
-                                        <input type="text" id="inputAddress" name="inputAddress" class="form-control" placeholder="Address" autofocus>
+                                        <input type="text" id="inputAddress" name="inputAddress" class="form-control" placeholder="Address" autofocus required>
                                         <label for="inputAddress">Dirección</label>
                                     </div>
                                 </div>
                                 <div class="col-sm-6">
                                     <div class="form-label-group">
-                                        <input type="password" id="inputPassword" name="inputPassword" class="form-control" placeholder="Password">
+                                        <input type="password" id="inputPassword" name="inputPassword" class="form-control" placeholder="Password" onkeydown="return /[a-z0-9!@#$%^&*_=+-]/i.test(event.key)" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8}$">
                                         <label for="inputPassword">Contraseña</label>
                                     </div>
                                 </div>
                                 <div class="col-sm-6">
                                     <div class="form-label-group">
-                                        <input type="password" id="inputRepeatPassword" name="inputRepeatPassword" class="form-control" placeholder="RepeatPassword">
+                                        <input type="password" id="inputRepeatPassword" name="inputRepeatPassword" maxlength="8" class="form-control" placeholder="RepeatPassword">
                                         <label for="inputRepeatPassword">Repetir Contraseña</label>
                                     </div>
                                 </div>
@@ -65,10 +65,13 @@
                         <p id="letter" class="invalid">Al menos una letra <b>minúscula</b></p>
                         <p id="capital" class="invalid">Al menos una letra <b>mayúscula</b></p>
                         <p id="number" class="invalid">Al menos un <b>número</b></p>
-                        <p id="length" class="invalid">Por lo menos <b>8 caracteres</b></p>
+                        <p id="length" class="invalid">La contraseña debe tener <b>8 caracteres</b></p>
+                        <p id="symbol" class="invalid">La contraseña debe incluir al menos uno de los siguientes caracteres especiales <b>!@#$%^&*_=+-</b></p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script type="text/javascript" src="assets/js/main.js"></script>
