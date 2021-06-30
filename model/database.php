7382c94520afda7d896122f8e922e2b2a08a4eea
ManuelRafael-P <manuelrafael2097@gmail.com>
2021-06-30 00:31:57 -0500

    update bd

--- model/database.php
+++ model/database.php
@@ -6,7 +6,7 @@ class Database
         $host = 'localhost';
         $db = 'proyecto_evo';
         $user = 'root';
-        $password = '';
+        $password = 'root';
         $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $db . ';charset=utf8', $user, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
